be7545f483b57b322f48a52cacd1dbc1985a7178
Joshua <joshuamoscosa13@gmail.com>
2020-08-02 17:23:49 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+require('./config/config');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json('Hello World');
+});
+
+app.get('/usuario', (req, res) => {
+    res.json('usuarios works')
+});
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+
+
+});
+
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+    res.json({
+        id,
+
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete usuarios works')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando peticiones en el puerto ${ process.env.PORT }`);
+});
